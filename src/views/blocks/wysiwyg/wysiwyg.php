d45a4d95bbc0ac02736c0e405bda889943a4578e
Mau <jmbaguio720@gmail.com>
2026-02-21 10:19:23 +0800

    wysiwyg

--- src/views/blocks/wysiwyg/wysiwyg.php
+++ src/views/blocks/wysiwyg/wysiwyg.php
@@ -7,27 +7,6 @@
  * @since SDEV WP Theme 2.0
  */  
 
-    // Support custom "anchor" values.
-    $anchor = '';
-    if ( ! empty( $block['anchor'] ) ) {
-        $anchor = 'id="' . esc_attr( $block['anchor'] ) . '" ';
-    }
-
-    // Get acf fields value and set default
-    $theme = get_field('theme') ?: 'light';
-    $background = get_field('background_color') ?: '#FFFFFF';
-    $content = get_field('content');
-
-    // Create class attribute allowing for custom "className" and "align" values.
-    $class_name = 'block--custom-layout__wysiwyg';
-    if ( ! empty( $block['className'] ) ) {
-        $class_name .= ' ' . $block['className'];
-    }
-    if ( ! empty( $block['align'] ) ) {
-        $class_name .= ' align' . $block['align'];
-    }
-
-    $class_name .= ' block-theme-'.$theme;
 
     // Show preview image in preview mode
     if(get_field('preview_image')) :
@@ -37,9 +16,11 @@
     else :
 ?>
 
-    <div class="block--custom-layout <?= $class_name ?>" <?= $anchor ?> style="background-color:<?= $background ?>;">
-        <div class="container-block">
-            <?= $content ?>
+    <div class="wysiwyg">
+        <div class="max-wrap margin-auto">
+            <div class="wysiwyg__wrap">
+                <?=get_field('wysiwyg_field')?>
+            </div>
         </div>
     </div>
 
